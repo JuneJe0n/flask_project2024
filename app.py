f28b2aeed52e1c88dad9265d2cee0a47cae0f395
ChoSeoyeon <5313lucky@ewhain.net>
2024-11-08 16:40:46 +0900

    1108 파이어베이스 수정 - firebase, app, authentication, reg_items

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for
+from database import DBhandler
 import os
 
 application = Flask(__name__)
+
+DB = DBhandler()
 
 # Initialize an empty list to store item data
 items = []
@@ -32,30 +35,16 @@ def reg_reviews():
     return render_template("reg_reviews.html")
 
 @application.route("/submit_item_post", methods=['POST'])
-def reg_item_submit_post():
+def submit_item_post():
+    print('#################')
     # Save the uploaded image file
     image_file = request.files["file"]
-    image_path = f"static/images/{image_file.filename}"
-    image_file.save(image_path)
+    image_file.save("static/image/{}".format(image_file.filename))
+    data = request.form
+    DB.insert_item(data['name'], data, image_file.filename)
 
-    # Collect form data and add it to the items list
-    data = {
-        'seller': request.form.get('seller'),
-        'name': request.form.get('name'),
-        'category': request.form.get('category'),
-        'status': request.form.get('status'),
-        'email': request.form.get('email'),
-        'phone': request.form.get('phone'),
-        'addr': request.form.get('addr'),
-        'card': request.form.get('card'),
-        'price': request.form.get('price'),
-        'rating': request.form.get('rating'),
-        'img_path': image_path
-    }
-    items.append(data)  # Append the new item to the items list
-
-    # Redirect to the list view after submission
-    return render_template("submit_item_result.html", data=data, img_path=image_path)
+    print (data['name'])
+    return render_template("submit_item_result.html", data=data, img_path="static/images/{}".format(image_file.filename))
 
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
