4e90499ceb1879c066df0e1a69943e408d93260e
heojiwon <zizifirst0318@gmail.com>
2024-11-15 18:34:23 +0900

    list, detail에 등록 데이터 연결

--- app.py
+++ app.py
@@ -70,9 +70,14 @@ def view_list():
         page=page, page_count=int((item_counts/per_page)+1),
         total=item_counts)
 
+
 @application.route('/dynamicrul/<varible_name>/')
 def DynamicUrl(varible_name):
     return str(varible_name)
+
+@application.route("/detail")
+def view_itemdetail():
+    return render_template("detail.html")
 
 @application.route("/view_detail/<name>/")
 def view_item_detail(name):
@@ -81,10 +86,6 @@ def view_item_detail(name):
     print("####data:",data)
     return render_template("detail.html", name=name, data=data)
 
-
-@application.route("/detail")
-def view_itemdetail():
-    return render_template("detail.html")
 
 @application.route("/review")
 def view_review():
@@ -114,7 +115,31 @@ def submit_item_post():
     
     DB.insert_item(data['name'], data, img_list, opt_list)
 
-    return render_template("list.html", items=items)
+
+    page = request.args.get("page", 0, type=int)
+    per_page=8 # item count to display per page
+    per_row=4 # item count to display per row
+    row_count=int(per_page/per_row)
+    start_idx=per_page*page
+    end_idx=per_page*(page+1)
+    data = DB.get_items() #read the table
+    item_counts=len(data)
+    data=dict(list(data.items())[start_idx:end_idx])
+    tot_count = len(data)
+    for i in range(row_count):#last row
+        if (i == row_count-1) and (tot_count%per_row != 0):
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:])
+        else: 
+            locals()['data_{}'.format(i)] = dict(list(data.items())[i*per_row:(i+1)*per_row])
+
+    return render_template( 
+        "list.html",
+        datas=data.items(), 
+        row1=locals()['data_0'].items(), 
+        row2=locals()['data_1'].items(), 
+        limit=per_page,
+        page=page, page_count=int((item_counts/per_page)+1),
+        total=item_counts)
 
 @application.route("/submit_review_post", methods=['POST'])
 def submit_review_post():
