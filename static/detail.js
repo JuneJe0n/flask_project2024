34cb91620ebfc53f51bde9dbb3e669ecbc3f46d6
heojiwon <zizifirst0318@gmail.com>
2024-11-26 14:01:37 +0900

    week12생성

--- static/detail.js
+++ static/detail.js
@@ -70,3 +70,56 @@ document.addEventListener('input', function(event) {
 
 // select 요소의 변경 이벤트에 함수 연결
 select.addEventListener('change', updateSelectedOptions);
+
+//찜 기능
+function showHeart() { $.ajax({
+    type: 'GET',
+    url: '/show_heart/{{name}}/',
+    data: {},
+    success: function (response) {
+        let my_heart = response['my_heart'];
+        if (my_heart['interested'] == 'Y')
+        {
+            $("#heart").css("color","red");
+            $("#heart").attr("onclick","unlike()");
+        }
+        else
+        {
+        $("#heart").css("color","grey");
+        $("#heart").attr("onclick","like()");
+    }
+        //alert("showheart!")
+        }
+    });
+}
+
+function like() {
+    $.ajax({
+        type: 'POST',
+        url: '/like/{{name}}/',
+        data: {
+            interested : "Y"
+        },
+        success: function (response) {
+            alert(response['msg']);
+            window.location.reload() 
+        }
+    });
+}
+
+function unlike() {
+    $.ajax({
+        type: 'POST',
+        url: '/unlike/{{name}}/',
+        data: {
+            interested : "N"
+        },
+        success: function (response) {
+            alert(response['msg']);
+            window.location.reload() 
+        }
+    });
+}
+$(document).ready(function () {
+    showHeart();
+});
