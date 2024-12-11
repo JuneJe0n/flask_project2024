c4bb4d66d5e9651d9f372ef92a159b24a92f1c57
heojiwon <zizifirst0318@gmail.com>
2024-12-11 23:45:41 +0900

    list.css를 like와 통일

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def view_myitems():
             locals()['data_{}'.format(i)] = dict(list(data.items())[i * per_row:(i + 1) * per_row])
 
     finalprices = {}
-    review_counts = 0
+    review_count = 0
     if data:
         for key, item in data.items():
             price = float(item.get('price', 0))
